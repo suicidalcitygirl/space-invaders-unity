2290beba92c204d0a7059732b92ed25ff5abf248
Uglypr1nces <shpatavdiu4@gmail.com>
2024-05-03 11:47:07 +0200

    added invisability after getting hit

--- Assets/Scripts/Player/PlayerDeath.cs
+++ Assets/Scripts/Player/PlayerDeath.cs
@@ -1,42 +1,45 @@
-
 using System;
-
 using UnityEngine;
 using UnityEngine.UI;
-
-public class PlayerDeath : MonoBehaviour {
+using System.Collections;
+public class PlayerDeath : MonoBehaviour
+{
 
     public static bool isDead = false;
-
-	public int hitPoints;
-	public string[] killingTags;
+    public bool invisible;
+    public int hitPoints;
+    public string[] killingTags;
     public GameObject objectToSpawnOnDestroy;
     public GameObject objectToSpawnOnDamage;
-	public GameObject gameOverMenu;
+    public GameObject gameOverMenu;
     public Slider HPBar;
 
-    private void Start () {
+    private void Start()
+    {
 
         isDead = false;
         HPBar.maxValue = hitPoints - 1;
     }
 
-	private void OnCollisionEnter2D (Collision2D collision) {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
 
-		bool damageTaken = false;
+        bool damageTaken = false;
+        if (killingTags.Length == 0) damageTaken = true;
+        else if (Array.IndexOf(killingTags, collision.collider.tag) > -1)
+            damageTaken = true;
 
-		if (killingTags.Length == 0) damageTaken = true;
-		else if (Array.IndexOf(killingTags, collision.collider.tag) > -1)
-			damageTaken = true;
-
-		if (damageTaken) {
-
-			hitPoints--;
+        if (damageTaken && !invisible)
+        {
+            invisible = true;
+            StartCoroutine(Timer(3));
+            hitPoints--;
             HPBar.value = hitPoints - 1;
 
-			if (hitPoints <= 0) {
+            if (hitPoints <= 0)
+            {
 
-				Instantiate(objectToSpawnOnDestroy, transform.position, transform.rotation);
+                Instantiate(objectToSpawnOnDestroy, transform.position, transform.rotation);
 
                 isDead = true;
                 gameOverMenu.SetActive(true);
@@ -44,8 +47,15 @@ public class PlayerDeath : MonoBehaviour {
 
                 Destroy(gameObject);
 
-            } else
+            }
+            else
                 Instantiate(objectToSpawnOnDamage, transform.position, transform.rotation);
-		}
-	}
+        }
+
+        IEnumerator Timer(float seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+            invisible = false;
+        }
+    }
 }
